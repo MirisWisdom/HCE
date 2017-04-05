d96d735c414fb16cd1610242871d92e38bb5a7e5
Roman Emilian <roman.emilian@outlook.com>
2017-04-05 12:33:07 +0800

    Adding background music and Launch button.
    Additional additions:
    
    * UI PSDs folder for Photoshop GUI-related documents.

--- spv3/legacy/promise/develop/Promise.UI/MainWindow.xaml.cs
+++ spv3/legacy/promise/develop/Promise.UI/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace Promise.UI
+﻿using System;
+using System.Media;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace Promise.UI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +13,36 @@
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LaunchButton_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            launchButton.Source = GetBitmapImage("Resources/Graphics/Buttons/launch_hover.png");
+        }
+
+        private void LaunchButton_OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            launchButton.Source = GetBitmapImage("Resources/Graphics/Buttons/launch.png");
+        }
+
+        private BitmapImage GetBitmapImage(string uri)
+        {
+            return new BitmapImage(new Uri(uri, UriKind.Relative));
+        }
+
+        private void backgroundMusic_MediaEnded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            backgroundMusic.Position = TimeSpan.Zero;
+            backgroundMusic.Play();
+        }
+
+
+
+        private void TriggerSoundEffect(string soundLocation)
+        {
+            throw new NotImplementedException();
+            //SoundPlayer soundPlayer = new SoundPlayer(soundLocation);
+            //soundPlayer.Play();
         }
     }
 }
