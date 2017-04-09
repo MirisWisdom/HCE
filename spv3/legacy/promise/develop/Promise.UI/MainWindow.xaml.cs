e458126a036386fd6f1c30cb10bb4154adc39418
Roman Emilian <roman.emilian@outlook.com>
2017-04-09 13:58:16 +0800

    Adding all of the main buttons.

--- spv3/legacy/promise/develop/Promise.UI/MainWindow.xaml.cs
+++ spv3/legacy/promise/develop/Promise.UI/MainWindow.xaml.cs
@@ -24,26 +24,59 @@ namespace Promise.UI
         }
 
         // Button events
-        private void LaunchButton_OnMouseEnter(object sender, MouseEventArgs e)
+        private void launchButton_OnMouseEnter(object sender, MouseEventArgs e)
         {
             ToggleHoverSoundEffect();
             launchButton.Source = GetBitmapImage("Resources/Graphics/Buttons/launch_hover.png");
         }
 
-        private void LaunchButton_OnMouseLeave(object sender, MouseEventArgs e)
+        private void launchButton_OnMouseLeave(object sender, MouseEventArgs e)
         {
             launchButton.Source = GetBitmapImage("Resources/Graphics/Buttons/launch.png");
         }
 
-        private void ConfigButton_MouseEnter(object sender, MouseEventArgs e)
+        private void configButton_MouseEnter(object sender, MouseEventArgs e)
         {
             ToggleHoverSoundEffect();
             configButton.Source = GetBitmapImage("Resources/Graphics/Buttons/config_hover.png");
         }
 
-        private void ConfigButton_MouseLeave(object sender, MouseEventArgs e)
+        private void configButton_MouseLeave(object sender, MouseEventArgs e)
         {
             configButton.Source = GetBitmapImage("Resources/Graphics/Buttons/config.png");
+        }
+
+        private void helpButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ToggleHoverSoundEffect();
+            helpButton.Source = GetBitmapImage("Resources/Graphics/Buttons/help_hover.png");
+        }
+
+        private void helpButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            helpButton.Source = GetBitmapImage("Resources/Graphics/Buttons/help.png");
+        }
+
+        private void aboutButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ToggleHoverSoundEffect();
+            aboutButton.Source = GetBitmapImage("Resources/Graphics/Buttons/about_hover.png");
+        }
+
+        private void aboutButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            aboutButton.Source = GetBitmapImage("Resources/Graphics/Buttons/about.png");
+        }
+
+        private void exitButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ToggleHoverSoundEffect();
+            exitButton.Source = GetBitmapImage("Resources/Graphics/Buttons/exit_hover.png");
+        }
+
+        private void exitButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            exitButton.Source = GetBitmapImage("Resources/Graphics/Buttons/exit.png");
         }
 
         // Helper functions
