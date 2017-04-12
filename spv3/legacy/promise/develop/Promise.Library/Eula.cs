4b23f15916f5a948e128aee22244e262849329bb
Roman Emilian <roman.emilian@outlook.com>
2017-04-12 11:26:09 +0800

    Adding Eula injection and basic unit tests.

---
+++ spv3/legacy/promise/develop/Promise.Library/Eula.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Promise.Library.Properties;
+
+namespace Promise.Library
+{
+    public class Eula
+    {
+        public const string EulaLibrary = "eula.dll";
+        public const string EulaDocument = "eula.rtf";
+
+        public void WriteEulaLibrary()
+        {
+            File.WriteAllBytes(EulaLibrary, Resources.EulaLib);
+        }
+
+        public void WriteEulaDocument()
+        {
+            File.WriteAllText(EulaDocument, Resources.EulaDoc);
+        }
+    }
+}
---
+++ spv3/legacy/promise/develop/Promise.Test/EulaTest.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Promise.Library;
+
+namespace Promise.Test
+{
+    [TestClass]
+    public class EulaTest
+    {
+        [TestMethod]
+        public void WriteEulaLibrary_LibraryIsDumped_True()
+        {
+            Eula eula = new Eula();
+            eula.WriteEulaLibrary();
+
+            Assert.AreEqual(File.Exists(Eula.EulaLibrary), true);
+        }
+
+        [TestMethod]
+        public void WriteEulaDocument_DocumentIsDumped_True()
+        {
+            Eula eula = new Eula();
+            eula.WriteEulaDocument();
+
+            Assert.AreEqual(File.Exists(Eula.EulaDocument), true);
+        }
+    }
+}
