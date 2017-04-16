d1a568d61ccb74e848c61baf19362c442dd7dc1a
Roman Emilian <roman.emilian@outlook.com>
2017-04-16 19:53:24 +0800

    Implementing OS Config Model & Controller.

---
+++ spv3/legacy/promise/develop/Promise.UI/Controller/OsConfigurationController.cs
@@ -0,0 +1,56 @@
+﻿using Promise.Library.OpenSauce;
+using Promise.Library.OpenSauce.Rasterizer;
+using Promise.Library.OpenSauce.Rasterizer.PostProcessing;
+using Promise.Library.OpenSauce.Rasterizer.ShaderExtensions;
+using Promise.UI.Model;
+using PropertyChanged;
+
+namespace Promise.UI.Controller
+{
+    [ImplementPropertyChanged]
+    internal class OsConfigurationController : OsConfiguration
+    {
+        public void SaveData()
+        {
+            OpenSauce openSauce = new OpenSauce
+            {
+                Camera = new Camera
+                {
+                    FieldOfView = FieldOfView,
+                    IgnoreFovChangeInCinematics = IsCinematicsFovIgnored,
+                    IgnoreFovChangeInMainMenu = IsMenuFovIgnored
+                },
+                Rasterizer = new Rasterizer
+                {
+                    GBuffer = new GBuffer { IsEnabled = true },
+                    Upgrades = new Upgrades { IsMaximumRenderedTrianglesEnabled = true },
+                    ShaderExtensions = new ShaderExtensions
+                    {
+                        IsEnabled = IsShader,
+                        Environment = new Environment
+                        {
+                            DiffuseDirectionalLightmaps = IsDiffuseDirectionalLightMaps,
+                            SpecularDirectionalLightmaps = IsDiffuseSpecularLightMaps
+                        },
+                        Effect = new Effect { IsDepthFadeEnabled = IsDepthFade }
+                    },
+                    PostProcessing = new PostProcessing
+                    {
+                        MotionBlur = new MotionBlur
+                        {
+                            IsEnabled = IsMotionBlur,
+                            BlurAmount = 0.005
+                        },
+                        Bloom = new Bloom { IsEnabled = IsBloom },
+                        AntiAliasing = new AntiAliasing { IsEnabled = IsAntiAliasing },
+                        ExternalEffects = new ExternalEffects() { IsEnabled = IsExternalEffects },
+                        MapEffects = new MapEffects { IsEnabled = IsMapEffects }
+                    }
+                }
+            };
+
+            OpenSauceConfiguration openSauceConfiguration = new OpenSauceConfiguration {OpenSauce = openSauce};
+            openSauceConfiguration.SaveData();
+        }
+    }
+}
---
+++ spv3/legacy/promise/develop/Promise.UI/Model/OsConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace Promise.UI.Model
+{
+    class OsConfiguration
+    {
+        public bool IsShader { get; set; } = true;
+        public bool IsNormalMaps { get; set; } = true;
+        public bool IsSpecularMaps { get; set; } = true;
+        public bool IsSpecularLighting { get; set; } = true;
+        public bool IsDiffuseDirectionalLightMaps { get; set; } = true;
+        public bool IsDiffuseSpecularLightMaps { get; set; } = true;
+        public bool IsDepthFade { get; set; } = true;
+
+        public bool IsMotionBlur { get; set; } = false;
+        public bool IsBloom { get; set; } = true;
+        public bool IsAntiAliasing { get; set; } = false;
+        public bool IsExternalEffects { get; set; } = true;
+        public bool IsMapEffects { get; set; } = true;
+
+        public int FieldOfView { get; set; } = 70;
+        public bool IsCinematicsFovIgnored { get; set; } = true;
+        public bool IsMenuFovIgnored { get; set; } = true;
+    }
+}
