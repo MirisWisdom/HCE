b4cce0c6acf119c08c8fcddfc553a17a1e541311
Roman Emilian <roman.emilian@outlook.com>
2017-04-19 18:16:50 +0800

    Implementing Launch class.

--- spv3/legacy/promise/develop/Promise.Library/Halo/Halo.cs
+++ spv3/legacy/promise/develop/Promise.Library/Halo/Halo.cs
@@ -1,14 +1,9 @@
-﻿using System.Diagnostics;
-using System.IO;
-using Promise.Library.Halo.Video;
-using Promise.Library.Utilities;
+﻿using Promise.Library.Halo.Video;
 
 namespace Promise.Library.Halo
 {
     public class Halo
     {
-        private const string ExeName = "haloce.exe";
-
         public VideoResolution VideoResolution { get; set; } = new VideoResolution();
         public VideoRefreshRate VideoRefreshRate { get; set; } = new VideoRefreshRate();
         public VideoAdapter VideoAdapter { get; set; } = new VideoAdapter();
@@ -16,13 +11,5 @@ namespace Promise.Library.Halo
         public bool IsWindow { get; set; } = false;
         public bool IsSafeMode { get; set; } = false;
         public bool IsFixedMode { get; set; } = false;
-
-        public void Launch(ConfigOperation configOperation)
-        {
-            if (File.Exists(ExeName))
-                Process.Start(ExeName, configOperation.ReadConfiguration());
-            else
-                throw new FileNotFoundException("Halo executable has not been found in this directory.");
-        }
     }
 }
---
+++ spv3/legacy/promise/develop/Promise.Library/Halo/Launch.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using System.IO;
+
+namespace Promise.Library.Halo
+{
+    public class Launch
+    {
+        private readonly Halo _halo;
+
+        private const string ExecutableName = "haloce.exe";
+
+        public Launch(Halo halo)
+        {
+            _halo = halo;
+        }
+
+        public void Start()
+        {
+            if (File.Exists(ExecutableName))
+                Process.Start(ExecutableName, GetLaunchParameters());
+            else
+                throw new FileNotFoundException("Halo executable has not been found in this directory.");
+        }
+
+        private string GetLaunchParameters()
+        {
+            string windowMode = _halo.IsWindow ? "-window" : string.Empty;
+            string safeMode = _halo.IsSafeMode ? "-safemode" : string.Empty;
+            string fixedMode = _halo.IsFixedMode ? "-useff" : string.Empty;
+
+            string resolution = $"{_halo.VideoResolution.Width },{_halo.VideoResolution.Height},{_halo.VideoRefreshRate.Rate}";
+            string toggles = $"{windowMode} {safeMode} {fixedMode}";
+
+            return $"-vidmode {resolution} -adapter {_halo.VideoAdapter.Index} {toggles}";
+        }
+    }
+}
