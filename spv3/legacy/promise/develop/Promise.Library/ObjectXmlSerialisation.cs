53ea828f5ee612953f3ef2544d32762cd05959da
Roman Emilian <roman.emilian@outlook.com>
2017-04-19 18:13:49 +0800

    Changing Halo & OS XML Serialisation classes to generic type.

---
+++ spv3/legacy/promise/develop/Promise.Library/ObjectXmlSerialisation.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Promise.Library
+{
+    public class ObjectXmlSerialisation<T>
+    {
+        public void SerialiseNewXml(T objectToSerialise, string fileName)
+        {
+            using (FileStream file = File.Create(fileName))
+                new XmlSerializer(objectToSerialise.GetType()).Serialize(file, objectToSerialise);
+        }
+
+        public T GetDeserialisedInstance(string configurationFileName)
+        { 
+            if (!File.Exists(configurationFileName))
+                throw new FileNotFoundException($"Could not find {configurationFileName} configuration file.");
+
+            using (XmlReader reader = XmlReader.Create(configurationFileName))
+                return (T)new XmlSerializer(typeof(T)).Deserialize(reader);
+        }
+    }
+}
