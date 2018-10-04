807a1097c0ed3198654ef0d6df6c3148ae9c72af
Emilian Roman <roman.emilian@outlook.com>
2018-10-05 05:53:02 +0800

    Implement executable validation & secure loading

---
+++ hce/legacy/atarashii/master/Atarashii.Tests/VerifierTest.cs
@@ -0,0 +1,22 @@
+using System;
+using System.IO;
+using Atarashii.Executable;
+using NUnit.Framework;
+
+namespace Atarashii.Tests
+{
+    [TestFixture]
+    public class VerifierTest
+    {
+        [Test]
+        public void VerifyValidExecutable_ValueIsTrue_True()
+        {
+            var executable = $"{new Guid().ToString()}.exe";
+            var verifier = new Verifier();
+            
+            File.WriteAllBytes(executable, new byte[0x24B000]);
+            Assert.IsTrue(verifier.Verify(executable));
+            File.Delete(executable);
+        }
+    }
+}
