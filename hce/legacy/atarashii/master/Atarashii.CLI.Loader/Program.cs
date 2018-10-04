28e38d8100b49b7b3ea8051bb986ece334357139
Emilian Roman <roman.emilian@outlook.com>
2018-10-05 04:57:49 +0800

    Develop basic foundation for loading the HCE executable

---
+++ hce/legacy/atarashii/master/Atarashii.CLI.Loader/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Atarashii.CLI.Loader
+{
+    /// <summary>
+    ///     CLI front-end for loading a HCE executable.
+    /// </summary>
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                ErrorExit($"The specified executable '{args[0]}' was not found.", 1);
+            }
+
+            var hceExe = args[0];
+            var loader = new Atarashii.Loader();
+
+            try
+            {
+                loader.Execute(hceExe);
+            }
+            catch (LoaderException e)
+            {
+                ErrorExit(e.Message, 2);
+            }
+            catch (Exception e)
+            {
+                ErrorExit(e.Message, 3);
+            }
+            
+            Console.WriteLine($"The specified executable '{hceExe}' has been loaded.");
+            Environment.Exit(0);
+        }
+
+        private static void ErrorExit(string error, int code)
+        {
+            Console.Error.WriteLine(error);
+            Environment.Exit(code);
+        }
+    }
+}
---
+++ hce/legacy/atarashii/master/Atarashii.Tests/LoaderTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Atarashii.Tests
+{
+    [TestFixture]
+    public class LoaderTests
+    {
+        [Test]
+        public void LoadNonExistentExecutable_ThrowsException_True()
+        {
+            var executable = $"{new Guid().ToString()}.exe";
+            var loader = new Loader();
+
+            var ex = Assert.Throws<LoaderException>(() => loader.Execute(executable));
+            Assert.That(ex.Message, Is.EqualTo($"The specified executable '{executable}' was not found."));
+        }
+    }
+}
