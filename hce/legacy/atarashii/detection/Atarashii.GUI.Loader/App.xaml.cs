e57dd238cd26ddab404bbb132320d9fbec231b9a
Emilian Roman <roman.emilian@outlook.com>
2018-10-05 10:54:59 +0800

    Set up foundation for the HCE Loader GUI

---
+++ hce/legacy/atarashii/detection/Atarashii.GUI.Loader/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Atarashii.GUI.Loader
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            new MainWindow().Show();
+        }
+    }
+}
---
+++ hce/legacy/atarashii/detection/Atarashii.GUI.Loader/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Atarashii.GUI.Loader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
