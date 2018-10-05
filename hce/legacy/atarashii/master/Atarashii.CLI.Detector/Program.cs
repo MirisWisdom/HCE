2e8166fe1ffe3277d3927ce361194cc074207513
Emilian Roman <roman.emilian@outlook.com>
2018-10-05 14:24:03 +0800

    Develop the CLI front-end for the HCE executable path detector

---
+++ hce/legacy/atarashii/master/Atarashii.CLI.Detector/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Atarashii.CLI.Detector
+{
+    /// <summary>
+    ///     CLI front-end for detecting the HCE executable.
+    /// </summary>
+    internal class Program
+    {
+        public static void Main()
+        {
+            var path = new Executable.Detector().Detect();
+
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                Console.Error.WriteLine("Legally installed executable not found.");
+                Environment.Exit(1);
+            }
+            else
+            {
+                Console.WriteLine(path);
+                Environment.Exit(0);
+            }
+        }
+    }
+}
