a770c76d72d88fc81239201a61be43e820375fe6
Emilian Roman <roman.emilian@outlook.com>
2018-10-05 14:19:16 +0800

    Implement HCE executable detection GUI

---
+++ hce/legacy/atarashii/unify/Atarashii.GUI.Detector/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Atarashii.GUI.Detector
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private readonly Main _main;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            _main = (Main) DataContext;
+        }
+
+        private void Detect(object sender, RoutedEventArgs e) => _main.DetectExecutablePath();
+    }
+}
