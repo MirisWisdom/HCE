a1ab522508b863deffc6bf5d147c477e1c23e5f9
Emilian Roman <roman.emilian@outlook.com>
2018-10-07 14:05:07 +0800

    Allow copying of the HCE executable detection result

--- hce/legacy/atarashii/detection/Atarashii.GUI.Detector/Main.cs
+++ hce/legacy/atarashii/detection/Atarashii.GUI.Detector/Main.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Windows;
 
 namespace Atarashii.GUI.Detector
 {
@@ -28,11 +29,27 @@ namespace Atarashii.GUI.Detector
             try
             {
                 DetectedPath = ExecutableFactory.Get(ExecutableFactory.Type.Detect).Path;
-                LogWindow.Output("Executable found!");
+                LogWindow.Output($"Executable found: {DetectedPath}");
             }
             catch (FileNotFoundException e)
             {
                 LogWindow.Output(e.Message);
+            }
+        }
+
+        /// <summary>
+        ///     Copies the detected HCE path to the clipboard.
+        /// </summary>
+        public void CopyToClipboard()
+        {
+            if (!string.IsNullOrWhiteSpace(DetectedPath))
+            {
+                Clipboard.SetText(DetectedPath);
+                LogWindow.Output("Copied detected HCE path to the clipboard.");
+            }
+            else
+            {
+                LogWindow.Output("Refusing to copy null result to the clipboard.");
             }
         }
     }
