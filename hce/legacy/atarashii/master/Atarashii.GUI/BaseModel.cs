d440bc23b18583ae628d540b7438afc3984f1311
Emilian Roman <roman.emilian@outlook.com>
2018-10-07 16:04:57 +0800

    Merge base application code into the base model

--- hce/legacy/atarashii/master/Atarashii.GUI/BaseModel.cs
+++ hce/legacy/atarashii/master/Atarashii.GUI/BaseModel.cs
@@ -1,6 +1,9 @@
+using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows;
 using Atarashii.GUI.Properties;
+using Microsoft.Win32;
 
 namespace Atarashii.GUI
 {
@@ -8,6 +11,30 @@ namespace Atarashii.GUI
     {
         public LogWindow LogWindow { get; set; }
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public void ShowLogWindow(Window window)
+        {
+            LogWindow = new LogWindow();
+            LogWindow.Show();
+            LogWindow.Left += window.Width * 1.5;
+        }
+
+        public string PickFile(string filter)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = filter
+            };
+
+            return openFileDialog.ShowDialog() == true
+                ? openFileDialog.FileName
+                : string.Empty;
+        }
+
+        public void Exit()
+        {
+            Environment.Exit(0);
+        }
 
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
