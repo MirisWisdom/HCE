39e9277b32c18325815e398b66481cb9e574e02b
Emilian Roman <roman.emilian@outlook.com>
2018-10-07 12:07:10 +0800

    Implement GUI library with Output Window

---
+++ hce/legacy/atarashii/master/Atarashii.GUI/LogWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace Atarashii.GUI
+{
+    /// <summary>
+    ///     Interaction logic for LogWindow.xaml
+    /// </summary>
+    public partial class LogWindow : Window
+    {
+        public LogWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Copy(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(OutputTextBox.Text);
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            var dlg = new SaveFileDialog
+            {
+                FileName = "atarashii",
+                DefaultExt = ".log",
+                Filter = "Atarashii Log (.log)|*.log"
+            };
+
+            var result = dlg.ShowDialog();
+
+            if (result == true) File.WriteAllText(dlg.FileName, OutputTextBox.Text);
+        }
+    }
+}
