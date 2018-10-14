e16a94671f4a588e780e06cf73f095d33f5b72e5
Emilian Roman <roman.emilian@outlook.com>
2018-10-14 16:41:36 +0800

    Allow Package to handle installation logic, and let OpenSauce be a wrapper

--- hce/legacy/atarashii/detection/Atarashii/Package.cs
+++ hce/legacy/atarashii/detection/Atarashii/Package.cs
@@ -3,27 +3,27 @@ using System.IO;
 namespace Atarashii
 {
     /// <summary>
-    ///     Package
+    ///     Archive installer and verifier.
     /// </summary>
     public class Package
     {
         public const string Directory = "Packages";
 
-        public Package(string name, string description, string destination)
+        public string ArchiveName { get; }
+        public string Description { get; }
+        public string Destination { get; }
+
+        public Package(string archiveName, string description, string destination)
         {
-            Name = name;
+            ArchiveName = archiveName;
             Description = description;
             Destination = destination;
         }
 
-        public string Name { get; }
-        public string Description { get; }
-        public string Destination { get; }
-
         public void Install()
         {
-            if (File.Exists(Path.Combine(Directory, Name)))
-                throw new FileNotFoundException($"{Name} does not exist in {Directory}");
+            if (!File.Exists(Path.Combine(Directory, ArchiveName)))
+                throw new PackageException("Cannot install specified package. Archive does not exist.");
         }
     }
 }
