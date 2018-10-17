71cc4b25d322161e29d1e9aab06c29b90ba875d7
Emilian Roman <roman.emilian@outlook.com>
2018-10-18 00:01:18 +0800

    Initialise foundation for the OpenSauce InstallerGUI

---
+++ hce/legacy/atarashii/detection/Atarashii.GUI.OpenSauce.Installer/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Atarashii.GUI.OpenSauce.Installer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ hce/legacy/atarashii/detection/Atarashii.GUI.OpenSauce.Installer/Main.cs
@@ -0,0 +1,14 @@
+namespace Atarashii.GUI.OpenSauce.Installer
+{
+    public class Main : BaseModel
+    {
+        public string InstallationPath { get; set; }
+        
+        public bool CanInstall { get; set; }
+
+        public void Install()
+        {
+            
+        }
+    }
+}
