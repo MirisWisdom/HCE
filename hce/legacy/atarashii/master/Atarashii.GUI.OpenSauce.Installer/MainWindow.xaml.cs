daeabfeb11e83353e636bcf0d6349ff23dddbae3
Emilian Roman <roman.emilian@outlook.com>
2018-10-18 00:01:18 +0800

    Initialise foundation for the OpenSauce InstallerGUI

---
+++ hce/legacy/atarashii/master/Atarashii.GUI.OpenSauce.Installer/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace Atarashii.GUI.OpenSauce.Installer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private readonly Main _main;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            _main = (Main) DataContext;
+            _main.ShowLogWindow(this);
+        }
+
+        private void MainWindow_OnClosing(object sender, CancelEventArgs e)
+        {
+            BaseModel.Exit();
+        }
+    }
+}
