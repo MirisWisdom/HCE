e5479d5a0b2c24463782017337cd48c0b606a873
Emilian Roman <roman.emilian@outlook.com>
2018-10-17 23:04:38 +0800

    Turn the Package Factory into an OpenSauceInstaller factory

---
+++ hce/legacy/atarashii/master/Atarashii/OpenSauceInstallerFactory.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Atarashii
+{
+    public class OpenSauceInstallerFactory
+    {
+        public enum Type
+        {
+            Default
+        }
+
+        private readonly string _installationPath;
+        private readonly ILogger _logger;
+
+        /// <summary>
+        ///     OpenSauceInstallerFactory constructor.
+        /// </summary>
+        /// <param name="installationPath">
+        ///     The HCE directory path -- used to install the OpenSauce library data to.
+        /// </param>
+        public OpenSauceInstallerFactory(string installationPath)
+        {
+            _installationPath = installationPath;
+        }
+
+        /// <inheritdoc />
+        /// <param name="installationPath">
+        ///     The HCE directory path -- used to install the OpenSauce library data to.
+        /// </param>
+        /// <param name="logger">
+        ///     Logging class for logging the packages' output.
+        /// </param>
+        public OpenSauceInstallerFactory(string installationPath, ILogger logger) : this(installationPath)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        ///     Retrieves OpenSauceInstaller instance.
+        /// </summary>
+        /// <param name="type">
+        ///     Type of OpenSauceInstaller instance.
+        /// </param>
+        /// <returns>
+        ///     OpenSauceInstaller instance for installing OpenSauce to the filesystem with the built-in packages.
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     Invalid OpenSauceInstaller type given.
+        /// </exception>
+        public OpenSauceInstaller Get(Type type = Type.Default)
+        {
+            switch (type)
+            {
+                case Type.Default:
+                    return new OpenSauceInstaller(_installationPath, GetOpenSaucePackages());
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(type), type, null);
+            }
+        }
+
+        /// <summary>
+        ///     Retrieve a list of packages that represent the OpenSauce installation data.
+        /// </summary>
+        /// <returns>
+        ///     A list of OpenSauce packages that replicate an original OS installation when installed.
+        ///     All of the packages are expected to be in the directory defined by the Package.Directory constant.
+        /// </returns>
+        private List<Package> GetOpenSaucePackages()
+        {
+            var guiDirPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            var usrDirPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+
+            var libPackage = Path.Combine(Package.Directory, "lib.pkg");
+            var guiPackage = Path.Combine(Package.Directory, "gui.pkg");
+            var usrPackage = Path.Combine(Package.Directory, "usr.pkg");
+
+            return new List<Package>
+            {
+                new Package(libPackage, "OpenSauce core and dependencies", _installationPath, _logger),
+                new Package(guiPackage, "In-game OpenSauce UI assets", guiDirPath, _logger),
+                new Package(usrPackage, "OpenSauce XML user configuration", usrDirPath, _logger)
+            };
+        }
+    }
+}
