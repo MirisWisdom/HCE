450e0b6b3f46cd395f771e649838ec9c970fd219
Emilian Roman <roman.emilian@outlook.com>
2018-10-27 19:08:57 +0800

    Commit Lastprof pre-parsing verifications

--- hce/legacy/atarashii/detection/Atarashii.Profile.CLI/Program.cs
+++ hce/legacy/atarashii/detection/Atarashii.Profile.CLI/Program.cs
@@ -15,9 +15,20 @@ namespace Atarashii.Profile.CLI
             ExitIfNilArgs(args);
             ShowMessage("Invoked parsing of " + args[0], MessageType.Info);
 
+            if (!File.Exists(args[0]))
+                ExitWithError("Given lastprof file does not exist.", 1);
+
+            var lastprof = new Lastprof(File.ReadAllText(args[0]));
+            var lastprofState = lastprof.Verify();
+
+            if (!lastprofState.IsValid)
+                ExitWithError(lastprofState.Reason, 2);
+
+            ShowMessage("Lastrof verification has passed.", MessageType.Success);
+
             try
             {
-                var result = new Lastprof(File.ReadAllText(args[0])).Parse();
+                string result = new Lastprof(File.ReadAllText(args[0])).Parse();
                 ShowMessage("Profile name successfully parsed:", MessageType.Success);
                 Console.WriteLine(result);
             }
