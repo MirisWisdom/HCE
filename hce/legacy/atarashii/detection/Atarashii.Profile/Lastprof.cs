3dfe5b31920ef9f6417501353e08a59b6b474b23
Emilian Roman <roman.emilian@outlook.com>
2018-10-27 19:08:28 +0800

    Implement IVerifiable in the Lastprof class

--- hce/legacy/atarashii/detection/Atarashii.Profile/Lastprof.cs
+++ hce/legacy/atarashii/detection/Atarashii.Profile/Lastprof.cs
@@ -1,6 +1,6 @@
 namespace Atarashii.Profile
 {
-    public class Lastprof
+    public class Lastprof : IVerifiable
     {
         /// <summary>
         ///     Official name of the lastprof text file.
@@ -29,6 +29,19 @@ namespace Atarashii.Profile
             _data = data;
         }
 
+        /// <inheritdoc />
+        /// <returns>
+        ///     False if:
+        ///     - Given lastprof string lacks valid signature.
+        /// </returns>
+        public Verification Verify()
+        {
+            if (!_data.Contains(Signature))
+                return new Verification(false, "Given lastprof string lacks valid signature.");
+
+            return new Verification(true);
+        }
+
         /// <summary>
         ///     Retrieves the profile name from a lastprof.txt string.
         /// </summary>
@@ -39,11 +52,14 @@ namespace Atarashii.Profile
         ///     The profile name. In actual environments, it's the profile used in the last HCE instance.
         /// </returns>
         /// <exception cref="ProfileException">
-        ///     Given Lastprof data is deemed as invalid.
+        ///     Given lastprof string lacks valid signature..
         /// </exception>
         public string Parse()
         {
-            if (!_data.Contains(Signature)) throw new ProfileException("Invalid lastprof string.");
+            var state = Verify();
+
+            if (!state.IsValid)
+                throw new ProfileException(state.Reason);
 
             var array = _data.Split(Delimiter);
             return array[array.Length - NameOffset];
