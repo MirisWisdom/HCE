50e42924b94f0d3bdedd60db0c5b24975fc40fb7
Emilian Roman <roman.emilian@outlook.com>
2018-10-28 16:42:02 +0800

    Improve the code and style of the CLI output

--- hce/legacy/atarashii/master/Atarashii.CLI/Common/Output.cs
+++ hce/legacy/atarashii/master/Atarashii.CLI/Common/Output.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
@@ -8,28 +9,79 @@ namespace Atarashii.CLI.Common
     ///     Abstract type representing console output data.
     /// </summary>
     public abstract class Output
-    {
+    {   
         /// <summary>
         ///     File information of the calling assembly.
         /// </summary>
         protected static readonly FileVersionInfo Exe =
             FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly().Location);
 
+        /// <summary>
+        ///     Git repository information.
+        /// </summary>
         protected static class Git
         {
-            private const string RevisionFile = "Atarashii.CLI.REVISION";
+            public static string Revision => ReadFromResource("Atarashii.CLI.REVISION");
+        }
 
-            public static string Revision
+        /// <summary>
+        ///     Reads data from an embedded resource.
+        /// </summary>
+        /// <param name="resourceName">
+        ///    Fully qualified embedded resource name.
+        /// </param>
+        /// <returns>
+        ///    Data read from the embedded resource.
+        /// </returns>
+        protected static string ReadFromResource(string resourceName)
+        {
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream))
             {
-                get
+                return reader.ReadToEnd();
+            }
+        }
+
+        /// <summary>
+        ///     Writes an indented line to the console.
+        /// </summary>
+        /// <param name="value">
+        ///    Value to write.
+        /// </param>
+        protected static void Write(string value)
+        {
+            Console.Write(new string(' ', 2) + value);
+        }
+
+        /// <summary>
+        ///     Writes an indented line to the console w/ newline at the end.
+        /// </summary>
+        /// <param name="value">
+        ///    Value to write.
+        /// </param>
+        protected static void WriteLine(string value)
+        {
+            Console.WriteLine(new string(' ', 2) + value);
+        }
+
+        /// <summary>
+        ///     Indents and writes an inbound string with multiple lines.
+        /// </summary>
+        /// <param name="value">
+        ///    Value to write.
+        /// </param>
+        protected static void WriteLineMulti(string value)
+        {
+            using (StringReader reader = new StringReader(value))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
                 {
-                    using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(RevisionFile))
-                    using (var reader = new StreamReader(stream))
-                    {
-                        return reader.ReadToEnd();
-                    }
+                    WriteLine(line);
                 }
             }
+            
+            WriteLine(string.Empty);
         }
     }
 }
