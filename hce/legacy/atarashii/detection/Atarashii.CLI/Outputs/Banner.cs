3ea1049b9c4298386c13c064e3c18f9e4b892ae7
Emilian Roman <roman.emilian@outlook.com>
2018-10-28 10:14:05 +0800

    Clean up the code in the Banner class

--- hce/legacy/atarashii/detection/Atarashii.CLI/Outputs/Banner.cs
+++ hce/legacy/atarashii/detection/Atarashii.CLI/Outputs/Banner.cs
@@ -8,16 +8,12 @@ namespace Atarashii.CLI.Outputs
         /// <summary>
         ///     CLI-friendly ASCII art banner.
         /// </summary>
-        private static string Art => $@"
+        private static string Art => @"
          _                      _     _ _ 
     __ _| |_ __ _ _ __ __ _ ___| |__ (_|_)
    / _` | __/ _` | '__/ _` / __| '_ \| | |
   | (_| | || (_| | | | (_| \__ \ | | | | |
    \__,_|\__\__,_|_|  \__,_|___/_| |_|_|_|
-
-  Program    : {Exe.ProductName}
-  Developers : {Exe.CompanyName}
-  Revision   : {Git.Revision}
 ";
 
         /// <summary>
@@ -25,8 +21,55 @@ namespace Atarashii.CLI.Outputs
         /// </summary>
         public static void Show()
         {
+            ShowAsciiBanner();
+            ShowProductName();
+            ShowCompanyName();
+            ShowGitRevision();
+        }
+
+        /// <summary>
+        ///     Outputs the ASCII art of the Atarashii CLI.
+        /// </summary>
+        private static void ShowAsciiBanner()
+        {
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine(Art);
+        }
+
+        /// <summary>
+        ///     Outputs the Product Name defined in the Assembly information.
+        /// </summary>
+        private static void ShowProductName()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("  Program    : ");
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(Exe.ProductName);
+        }
+
+        /// <summary>
+        ///     Outputs the Company Name defined in the Assembly information.
+        /// </summary>
+        private static void ShowCompanyName()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("  Developers : ");
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(Exe.CompanyName);
+        }
+
+        /// <summary>
+        ///     Outputs the Git revision that was used to build the binary.
+        /// </summary>
+        private static void ShowGitRevision()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("  Revision   : ");
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(Git.Revision);
         }
     }
 }
