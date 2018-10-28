d86f09ce03b0ff0c3783a1b7e2fdfe6c5338813e
Emilian Roman <roman.emilian@outlook.com>
2018-10-28 10:07:18 +0800

    Implement embedded Git revision display

--- hce/legacy/atarashii/detection/Atarashii.CLI/Common/Output.cs
+++ hce/legacy/atarashii/detection/Atarashii.CLI/Common/Output.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.IO;
 using System.Reflection;
 
 namespace Atarashii.CLI.Common
@@ -11,7 +12,22 @@ namespace Atarashii.CLI.Common
         /// <summary>
         ///     File information of the calling assembly.
         /// </summary>
-        protected static readonly FileVersionInfo Info =
+        protected static readonly FileVersionInfo Exe =
             FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly().Location);
+
+        protected static class Git
+        {
+            private const string RevisionFile = "Atarashii.CLI.REVISION";
+
+            public static string Revision
+            {
+                get
+                {
+                    using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(RevisionFile))
+                    using (var reader = new StreamReader(stream))
+                        return reader.ReadToEnd();
+                }
+            }
+        }
     }
 }
