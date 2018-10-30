1082a4f7821855c24fc1fee8aa65b4ff9c66d65f
Emilian Roman <roman.emilian@outlook.com>
2018-10-30 08:47:24 +0800

    Transform sub-command classes into methods

--- hce/legacy/atarashii/master/Atarashii.CLI/Commands/Loader.cs
+++ hce/legacy/atarashii/master/Atarashii.CLI/Commands/Loader.cs
@@ -1,10 +1,14 @@
-﻿namespace Atarashii.CLI.Commands
+﻿using System;
+using System.IO;
+using Atarashii.Loader;
+
+namespace Atarashii.CLI.Commands
 {
     /// <inheritdoc />
     /// <summary>
     ///     CLI front-end for loading a HCE executable.
     /// </summary>
-    internal partial class Loader : Command
+    internal class Loader : Command
     {
         private readonly Atarashii.Output _output;
 
@@ -22,15 +26,66 @@
             {
                 case nameof(Load):
                     HandleInvokeType(InvokeType.Success, this, nameof(Load));
-                    new Load(_output).Initialise(args);
+                    Load(args);
                     break;
                 case nameof(Detect):
                     HandleInvokeType(InvokeType.Success, this, nameof(Detect));
-                    new Detect(_output).Initialise(args);
+                    Detect();
                     break;
                 default:
                     HandleInvokeType(InvokeType.Invalid, this, commands[0]);
                     break;
+            }
+        }
+
+        private void Load(string[] args)
+        {
+            Argument.ExitIfNone(args);
+
+            var executable = new Executable(args[0]);
+            var executableState = executable.Verify();
+
+            if (executableState.IsValid)
+                _output?.Write(Atarashii.Output.Type.Success, $"{nameof(Loader)}.{nameof(Load)}",
+                    "Executable verification has passed.");
+            else
+                Exit.WithError(executableState.Reason, 5);
+
+            try
+            {
+                executable.Load();
+                _output?.Write(Atarashii.Output.Type.Success, Assembly.ProductName,
+                    "The specified executable has been loaded.");
+            }
+            catch (LoaderException e)
+            {
+                Exit.WithError(e.Message, 3);
+            }
+            catch (Exception e)
+            {
+                Exit.WithError(e.Message, 4);
+            }
+
+            Environment.Exit(0);
+        }
+
+        private void Detect()
+        {
+            _output?.Write(Atarashii.Output.Type.Info, $"{nameof(Loader)}.{nameof(Detect)}",
+                "Attempting to detect executable path.");
+
+            try
+            {
+                var result = ExecutableFactory.Get(ExecutableFactory.Type.Detect);
+                _output?.Write(Atarashii.Output.Type.Success, $"{nameof(Loader)}.{nameof(Detect)}",
+                    "Profile name successfully parsed:");
+                Console.WriteLine(result);
+                Environment.Exit(0);
+            }
+            catch (FileNotFoundException e)
+            {
+                Exit.WithError(e.Message, 3);
+                Environment.Exit(5);
             }
         }
     }
--- hce/legacy/atarashii/master/Atarashii.CLI/Commands/OpenSauce.cs
+++ hce/legacy/atarashii/master/Atarashii.CLI/Commands/OpenSauce.cs
@@ -1,7 +1,10 @@
-﻿namespace Atarashii.CLI.Commands
+﻿using System;
+using Atarashii.OpenSauce;
+
+namespace Atarashii.CLI.Commands
 {
     /// <inheritdoc />
-    internal partial class OpenSauce : Command
+    internal class OpenSauce : Command
     {
         private readonly Atarashii.Output _output;
 
@@ -19,11 +22,40 @@
             {
                 case nameof(Install):
                     HandleInvokeType(InvokeType.Success, this, nameof(Install));
-                    new Install(_output).Initialise(args);
+                    Install(args);
                     break;
                 default:
                     HandleInvokeType(InvokeType.Invalid, this, commands[0]);
                     break;
+            }
+        }
+
+        private void Install(string[] args)
+        {
+            Argument.ExitIfNone(args);
+
+            var installer = new InstallerFactory(args[0]).Get();
+            var installerState = installer.Verify();
+
+            if (installerState.IsValid)
+                _output?.Write(Atarashii.Output.Type.Success, $"{nameof(OpenSauce)}.Install",
+                    "Installer verification has passed.");
+            else
+                Exit.WithError(installerState.Reason, 4);
+
+            try
+            {
+                installer.Install();
+                _output?.Write(Atarashii.Output.Type.Success, $"{nameof(OpenSauce)}.Install",
+                    "OpenSauce has been successfully installed.");
+            }
+            catch (OpenSauceException e)
+            {
+                Exit.WithError(e.Message, 2);
+            }
+            catch (Exception e)
+            {
+                Exit.WithError(e.Message, 3);
             }
         }
     }
