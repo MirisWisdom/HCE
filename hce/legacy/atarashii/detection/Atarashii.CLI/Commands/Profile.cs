4e902b63e0cdabcb8d49f6f59151993e24bb8a23
Emilian Roman <roman.emilian@outlook.com>
2018-10-30 08:47:24 +0800

    Transform sub-command classes into methods

--- hce/legacy/atarashii/detection/Atarashii.CLI/Commands/Profile.cs
+++ hce/legacy/atarashii/detection/Atarashii.CLI/Commands/Profile.cs
@@ -1,10 +1,14 @@
-﻿namespace Atarashii.CLI.Commands
+﻿using System;
+using System.IO;
+using Atarashii.Profile;
+
+namespace Atarashii.CLI.Commands
 {
     /// <inheritdoc />
     /// <summary>
     ///     CLI front-end for loading a lastprof.txt file.
     /// </summary>
-    internal partial class Profile : Command
+    internal class Profile : Command
     {
         private readonly Atarashii.Output _output;
 
@@ -22,11 +26,39 @@
             {
                 case nameof(Resolve):
                     HandleInvokeType(InvokeType.Success, this, nameof(Resolve));
-                    new Resolve(_output).Initialise(args);
+                    Resolve(args);
                     break;
                 default:
                     HandleInvokeType(InvokeType.Invalid, this, commands[0]);
                     break;
+            }
+        }
+        
+        private void Resolve(string[] args)
+        {
+            Argument.ExitIfNone(args);
+
+            if (!File.Exists(args[0])) Exit.WithError("Given lastprof file does not exist.", 1);
+
+            var lastprof = new Lastprof(File.ReadAllText(args[0]));
+            var lastprofState = lastprof.Verify();
+
+            if (lastprofState.IsValid)
+                _output.Write(Atarashii.Output.Type.Success, $"{nameof(Profile)}.{nameof(Resolve)}",
+                    "Lastrof verification has passed.");
+            else
+                Exit.WithError(lastprofState.Reason, 2);
+
+            try
+            {
+                var result = new Lastprof(File.ReadAllText(args[0])).Parse();
+                _output.Write(Atarashii.Output.Type.Success, $"{nameof(Profile)}.{nameof(Resolve)}",
+                    "Profile name successfully parsed:");
+                Console.WriteLine(result);
+            }
+            catch (ProfileException e)
+            {
+                Exit.WithError(e.Message, 3);
             }
         }
     }
