cfc414b0746f76e60caed16bb50d3cd28f648ad5
Emilian Roman <roman.emilian@outlook.com>
2018-11-15 14:43:50 +0800

    Implement Mouse & Sensitivity documentation + validation

--- hce/legacy/atarashii/detection/Atarashii/Modules/Profile/Configuration.cs
+++ hce/legacy/atarashii/detection/Atarashii/Modules/Profile/Configuration.cs
@@ -2,16 +2,41 @@ using Atarashii.Modules.Profile.Options;
 
 namespace Atarashii.Modules.Profile
 {
+    // TODO: Controls/gamepad settings.
+    
     /// <summary>
     ///     This type is used to represent a HCE profile configuration.
     /// </summary>
     public class Configuration
     {
+        /// <summary>
+        ///     Player name.
+        /// </summary>
         public Name Name { get; set; } = new Name();
+        
+        /// <summary>
+        ///     Player colour.
+        /// </summary>
         public Colour Colour { get; set; } = new Colour();
+        
+        /// <summary>
+        ///     Mouse settings.
+        /// </summary>
         public Mouse Mouse { get; set; } = new Mouse();
+        
+        /// <summary>
+        ///     Audio settings.
+        /// </summary>
         public Audio Audio { get; set; } = new Audio();
+        
+        /// <summary>
+        ///     Video settings.
+        /// </summary>
         public Video Video { get; set; } = new Video();
+        
+        /// <summary>
+        ///     Network settings.
+        /// </summary>
         public Network Network { get; set; } = new Network();
     }
 }
