019c4a5a7797b4c96206b786a1478fe8c41a03c1
Emilian Roman <roman.emilian@outlook.com>
2018-11-17 06:38:07 +0800

    Introduce API support for HCE profile parsing & detection

--- hce/legacy/atarashii/master/Atarashii.API/OpenSauce.cs
+++ hce/legacy/atarashii/master/Atarashii.API/OpenSauce.cs
@@ -9,13 +9,13 @@ namespace Atarashii.API
     public static class OpenSauce
     {
         /// <summary>
-        ///     Retrieves a Configuration-type representation of the inbound path.
+        ///     Retrieves a Configuration-type representation of the provided OpenSauce User configuration XML file.
         /// </summary>
         /// <param name="path">
-        ///    Path to the OpenSauce User configuration XML file.
+        ///     Path to the OpenSauce User configuration XML file.
         /// </param>
         /// <returns>
-        ///    Deserialised Configuration object. 
+        ///     Deserialised Configuration object representing the OpenSauce User configuration XML file.
         /// </returns>
         public static Configuration Parse(string path)
         {
@@ -23,11 +23,11 @@ namespace Atarashii.API
         }
 
         /// <summary>
-        ///     Installs OpenSauce to the file system.
+        ///     Installs OpenSauce to the filesystem.
         /// </summary>
         /// <param name="path">
-        ///    HCE installation directory.
-        ///    This path is expected to contain a valid HCE executable.
+        ///     HCE installation directory.
+        ///     This path is expected to contain a valid HCE executable.
         /// </param>
         public static void Install(string path)
         {
