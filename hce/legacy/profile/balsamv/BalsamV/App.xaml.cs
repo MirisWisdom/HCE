873b1e56f3611f056859d9de8ee9a0c74e14c0a1
Emilian Roman <roman.emilian@outlook.com>
2018-11-21 17:11:22 +0800

    Initialise repository

---
+++ hce/legacy/profile/balsamv/BalsamV/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BalsamV
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ hce/legacy/profile/balsamv/BalsamV/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace BalsamV
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
