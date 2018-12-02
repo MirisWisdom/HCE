cfdba578ff9db68d3f26484e034bfc1239944bd0
Emilian Roman <roman.emilian@outlook.com>
2018-12-03 00:45:37 +0800

    Implement Global Variable Factory for encoding a Configuration to an int

--- spv3/legacy/shaders/master/src/SPV3.Shaders.Test/GlobalVariableTest.cs
+++ spv3/legacy/shaders/master/src/SPV3.Shaders.Test/GlobalVariableTest.cs
@@ -1,5 +1,6 @@
 using System;
 using NUnit.Framework;
+using SPV3.Shaders.Options;
 
 namespace SPV3.Shaders.Test
 {
@@ -16,6 +17,23 @@ namespace SPV3.Shaders.Test
         public void AssertException_SmallNumberIsAssigned_True()
         {
             Assert.Throws<ArgumentException>(() => new GlobalVariable(0));
+        }
+
+        [Test]
+        public void AssertProperty_IsEncodedCorrectly_True()
+        {
+            var configuration = new Configuration
+            {
+                AmbientOcclusion = {Level = Level.High},
+                DepthOfField = {Level = Level.Low},
+                DynamicFlare = {Toggle = Toggle.On},
+                LensDirt = {Toggle = Toggle.On},
+                EyeAdaption = {Toggle = Toggle.Off},
+                AntiAliasing = {Toggle = Toggle.On},
+                Debanding = {Level = Level.Low}
+            };
+            
+            Assert.AreEqual(42644, GlobalVariableFactory.Encode(configuration).Value);
         }
     }
 }
