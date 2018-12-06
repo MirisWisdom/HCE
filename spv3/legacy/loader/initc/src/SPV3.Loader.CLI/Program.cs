9473b7ed29f7e486dc1d2c21179910a3d609c638
Emilian Roman <roman.emilian@outlook.com>
2018-12-07 06:39:48 +0800

    Handle empty args on CLI initiation & explicit params/HCE path provision

--- spv3/legacy/loader/initc/src/SPV3.Loader.CLI/Program.cs
+++ spv3/legacy/loader/initc/src/SPV3.Loader.CLI/Program.cs
@@ -1,11 +1,33 @@
-﻿namespace SPV3.Loader.CLI
+﻿using System;
+
+namespace SPV3.Loader.CLI
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            new Loader(new LoaderConfiguration()).Start(ExecutableFactory.Detect(),
-                new ParametersParser().Parse(string.Join(" ", args)));
+            var config = new LoaderConfiguration();
+            var loader = new Loader(config);
+
+            try
+            {
+                if (args.Length == 0)
+                {
+                    loader.Start(ExecutableFactory.Detect());
+                    return;
+                }
+
+                var parameters = new ParametersParser().Parse(string.Join(" ", args));
+                var executable = args[0].Contains(Executable.Name)
+                    ? new Executable(args[0])
+                    : ExecutableFactory.Detect();
+
+                loader.Start(executable, parameters);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
