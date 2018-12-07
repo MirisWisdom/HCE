ebc1edabee233725b07bb43210a9c92657abbaa7
Emilian Roman <roman.emilian@outlook.com>
2018-12-07 09:29:27 +0800

    [Configuration/GlobalVariable]Factory => Configuration[Encoder/Decoder]

---
+++ spv3/legacy/shaders/gui/src/SPV3.Shaders/Debanding.cs
@@ -0,0 +1,18 @@
+namespace SPV3.Shaders
+{
+    /// <summary>
+    ///     Effect which eliminated banding artifacts (sort of band like anomalies that appear in otherwise smooth
+    ///     gradients of colors, due to small color space/bit depth).
+    /// </summary>
+    public class Debanding
+    {
+        public const int StateOff = 0x4000;
+        public const int StateLow = 0x8000;
+        public const int StateHigh = 0x10000;
+
+        /// <summary>
+        ///     <see cref="Level" />
+        /// </summary>
+        public Level Level { get; set; }
+    }
+}
---
+++ spv3/legacy/shaders/gui/src/SPV3.Shaders/DepthOfField.cs
@@ -0,0 +1,19 @@
+namespace SPV3.Shaders
+{
+    /// <summary>
+    ///     Effect in which objects within some range of distances in a scene appear in focus, and objects nearer or
+    ///     farther than this range appear out of focus or blurred out. Just like any camera or your eye, ADOF
+    ///     enables auto-focusing.
+    /// </summary>
+    public class DepthOfField
+    {
+        public const int StateOff = 0x8;
+        public const int StateLow = 0x10;
+        public const int StateHigh = 0x20;
+
+        /// <summary>
+        ///     <see cref="Level" />
+        /// </summary>
+        public Level Level { get; set; }
+    }
+}
