42dfe19918155873c32ddcb14b7d890556859dbd
Emilian Roman <roman.emilian@outlook.com>
2018-12-07 09:52:20 +0800

    Add functionality for each shader configuration button in the GUI

--- spv3/legacy/shaders/master/src/SVP3.Shaders.GUI/Main.cs
+++ spv3/legacy/shaders/master/src/SVP3.Shaders.GUI/Main.cs
@@ -1,15 +1,9 @@
-using System.Windows;
 using SPV3.Shaders;
 
 namespace SVP3.Shaders.GUI
 {
     public class Main
     {
-        public Configuration Configuration { get; set; }
-
-        public void Save()
-        {
-            MessageBox.Show($"{Configuration}");
-        }
+        public Configuration Configuration { get; set; } = new Configuration();
     }
 }
