f48a4bd16b2940014018694d77da9e04cb2e3246
Emilian Roman <roman.emilian@outlook.com>
2018-12-08 06:23:45 +0800

    Define the solution & fundamental entities

---
+++ spv3/legacy/installer/basedir/src/SPV3.Installer/Description.cs
@@ -0,0 +1,13 @@
+namespace SPV3.Installer
+{
+    /// <summary>
+    ///     Represents an user-friendly description of an entity (e.g. File or Package).
+    /// </summary>
+    public class Description
+    {
+        /// <example>
+        ///     SPV3.2 Maps for HCE
+        /// </example>
+        public string Value { get; set; }
+    }
+}
