18c85dba534943796ada80c7c1f0e3ec0f5dcad9
Emilian Roman <roman.emilian@outlook.com>
2018-12-07 11:55:49 +0800

    Develop the SPV3.Settings GUI layout

--- spv3/legacy/settings/src/SPV3.Settings.GUI/MainWindow.xaml.cs
+++ spv3/legacy/settings/src/SPV3.Settings.GUI/MainWindow.xaml.cs
@@ -1,27 +1,73 @@
-﻿using System.Diagnostics;
-using System.IO;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Windows;
+using BalsamV.Profile;
+using BalsamV.Settings;
+using Microsoft.Win32;
 
 namespace SPV3.Settings.GUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
+        private readonly Main _main;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitialiseComboBoxes();
+
+            _main = (Main) DataContext;
+            _main.Initialise();
         }
 
-        private void HceSettings(object sender, RoutedEventArgs e)
+        /// <summary>
+        ///     Assigns the Atarashii enumerators to the combo boxes.
+        /// </summary>
+        private void InitialiseComboBoxes()
         {
-            if (File.Exists("BalsamV.GUI.exe")) Process.Start("BalsamV.GUI.exe");
+            FrameRateComboBox.ItemsSource = Enum.GetValues(typeof(FrameRate)).Cast<FrameRate>();
+            TextureQualityComboBox.ItemsSource = Enum.GetValues(typeof(Quality)).Cast<Quality>();
+            ParticlesComboBox.ItemsSource = Enum.GetValues(typeof(Particles)).Cast<Particles>();
+            AudioQualityComboBox.ItemsSource = Enum.GetValues(typeof(Quality)).Cast<Quality>();
+            VarietyComboBox.ItemsSource = Enum.GetValues(typeof(Quality)).Cast<Quality>();
         }
 
-        private void Spv3Shaders(object sender, RoutedEventArgs e)
+        /// <summary>
+        ///     Invokes the blam.sav file picker.
+        /// </summary>
+        private void Load(object sender, RoutedEventArgs routedEventArgs)
         {
-            if (File.Exists("SPV3.Shaders.GUI.exe")) Process.Start("SPV3.Shaders.GUI.exe");
+            var dialog = new OpenFileDialog
+            {
+                Filter = "Profile Binary|blam.sav"
+            };
+
+            if (dialog.ShowDialog() == true)
+                _main.Path = dialog.FileName;
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            _main.Save();
+        }
+
+        private void About(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/yumiris/HCE.BalsamV");
+        }
+
+        private void Releases(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/yumiris/HCE.BalsamV/releases");
+        }
+
+        private void Version(object sender, RoutedEventArgs e)
+        {
+            Process.Start($"https://github.com/yumiris/HCE.BalsamV/releases/{_main.Version}");
         }
     }
 }
