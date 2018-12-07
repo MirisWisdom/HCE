b31a7e071b26885ad8a6b8e8e8594292699391da
Emilian Roman <roman.emilian@outlook.com>
2018-12-07 10:01:07 +0800

    Introduce rudimentary saving mechanism

--- spv3/legacy/shaders/master/src/SVP3.Shaders.GUI/Main.cs
+++ spv3/legacy/shaders/master/src/SVP3.Shaders.GUI/Main.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using SPV3.Shaders;
 
 namespace SVP3.Shaders.GUI
@@ -5,5 +6,11 @@ namespace SVP3.Shaders.GUI
     public class Main
     {
         public Configuration Configuration { get; set; } = new Configuration();
+
+        public void Save()
+        {
+            using (var writer = new StreamWriter("initc.txt"))
+                writer.WriteLine($"f0 = {ConfigurationEncoder.Encode(Configuration).Value}");
+        }
     }
 }
