73d2cb1f23c760c32d562888de1740b0bcf05bf8
Emilian Roman <roman.emilian@outlook.com>
2018-12-27 04:05:19 +0800

    Remove unused declarations in the project

--- spv3/legacy/installer/basedir/src/SPV3.Compiler.GUI/MainWindow.xaml.cs
+++ spv3/legacy/installer/basedir/src/SPV3.Compiler.GUI/MainWindow.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Windows;
-using Microsoft.Win32;
+﻿using System.Windows;
 
 namespace SPV3.Compiler.GUI
 {
@@ -10,81 +7,9 @@ namespace SPV3.Compiler.GUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly Main _main;
-
         public MainWindow()
         {
             InitializeComponent();
-            _main = (Main) DataContext;
-        }
-
-        private void CreatePackage(object sender, RoutedEventArgs e)
-        {
-            _main.SavePackage();
-        }
-
-        private void CreateFile(object sender, RoutedEventArgs e)
-        {
-            _main.SaveFile();
-        }
-
-        private void DeleteFile(object sender, RoutedEventArgs e)
-        {
-            _main.DeleteFile();
-        }
-
-        private void DeletePackage(object sender, RoutedEventArgs e)
-        {
-            _main.DeletePackage();
-        }
-
-        private void BatchFiles(object sender, RoutedEventArgs e)
-        {
-            var dialog = new OpenFileDialog
-            {
-                Multiselect = true,
-                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
-            };
-
-            if (dialog.ShowDialog() != true) return;
-
-            foreach (var filename in dialog.FileNames)
-            {
-                _main.SelectedPackage.Files.Add(new Main.File
-                {
-                    Name = Path.GetFileName(filename),
-                    Description = $"Data for {Path.GetFileName(filename)}"
-                });
-            }
-        }
-
-        private void CreateInstaller(object sender, RoutedEventArgs e)
-        {
-            var dialog = new SaveFileDialog
-            {
-                FileName = "metadata.bin",
-                Filter = "Installer Metadata|metadata.bin"
-            };
-
-            if (dialog.ShowDialog() == true)
-                _main.SaveTo(dialog.FileName);
-        }
-
-        private void DropFiles(object sender, DragEventArgs e)
-        {
-            var files = (string[]) e.Data.GetData(DataFormats.FileDrop);
-
-            if (files == null) return;
-            if (files.Length == 0) return;
-
-            foreach (var file in files)
-            {
-                _main.SelectedPackage.Files.Add(new Main.File
-                {
-                    Name = Path.GetFileName(file),
-                    Description = $"Data for {Path.GetFileName(file)}"
-                });
-            }
         }
     }
 }
