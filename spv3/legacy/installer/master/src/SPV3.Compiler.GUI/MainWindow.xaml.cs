b798b2c0f261c9841cc82787036dd90c0fd97660
Emilian Roman <roman.emilian@outlook.com>
2018-12-27 23:16:27 +0800

    Implement properties and data binding

--- spv3/legacy/installer/master/src/SPV3.Compiler.GUI/MainWindow.xaml.cs
+++ spv3/legacy/installer/master/src/SPV3.Compiler.GUI/MainWindow.xaml.cs
@@ -7,9 +7,35 @@ namespace SPV3.Compiler.GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Main _main;
+        
         public MainWindow()
         {
             InitializeComponent();
+            _main = (Main) DataContext;
+        }
+
+        private void BrowseSource(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                dialog.ShowDialog();
+                _main.Source = dialog.SelectedPath;
+            }
+        }
+
+        private void BrowseTarget(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                dialog.ShowDialog();
+                _main.Target = dialog.SelectedPath;
+            }
+        }
+
+        private void Create(object sender, RoutedEventArgs e)
+        {
+            _main.Create();
         }
     }
 }
