c392c61d5e2742cfcac8f1fef8d2bce1282df0bd
Emilian Roman <roman.emilian@outlook.com>
2018-12-28 05:10:44 +0800

    Introduce the Package & Entry entities and tests

---
+++ spv3/legacy/domain/src/SPV3.Domain/Entry.cs
@@ -0,0 +1,44 @@
+namespace SPV3.Domain
+{
+    /// <summary>
+    ///     Represents an entry within a Package. <see cref="Package"/>
+    /// </summary>
+    public class Entry
+    {
+        /// <summary>
+        ///     <see cref="Name" />
+        /// </summary>
+        public Name Name { get; set; }
+
+        /// <summary>
+        ///     Represent object as string.
+        /// </summary>
+        /// <param name="entry">
+        ///     Object to represent as string.
+        /// </param>
+        /// <returns>
+        ///     String representation of the object.
+        /// </returns>
+        public static implicit operator string(Entry entry)
+        {
+            return entry.Name;
+        }
+
+        /// <summary>
+        ///     Represent string as object.
+        /// </summary>
+        /// <param name="value">
+        ///     String to represent as object.
+        /// </param>
+        /// <returns>
+        ///     Object representation of the string.
+        /// </returns>
+        public static explicit operator Entry(string value)
+        {
+            return new Entry
+            {
+                Name = (Name) value
+            };
+        }
+    }
+}
