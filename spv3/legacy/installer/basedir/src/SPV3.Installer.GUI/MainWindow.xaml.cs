0401d5051b9dfed3f6dadf4485d2118ada4d5a1d
Emilian Roman <roman.emilian@outlook.com>
2018-12-30 07:02:19 +0800

    Implement rudimentary GUI & async installation routine

---
+++ spv3/legacy/installer/basedir/src/SPV3.Installer.GUI/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace SPV3.Installer.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private Main _main;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            _main = (Main) DataContext;
+        }
+
+        private void Install(object sender, RoutedEventArgs e)
+        {
+            _main.Install();
+        }
+    }
+}
