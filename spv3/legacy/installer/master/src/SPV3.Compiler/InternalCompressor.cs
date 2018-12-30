2d9ec77420ec57f6c3c20a7bb1588ffb88c39a94
Emilian Roman <roman.emilian@outlook.com>
2018-12-31 06:13:10 +0800

    Tweak comments & formatting

--- spv3/legacy/installer/master/src/SPV3.Compiler/InternalCompressor.cs
+++ spv3/legacy/installer/master/src/SPV3.Compiler/InternalCompressor.cs
@@ -9,32 +9,13 @@ namespace SPV3.Compiler
     /// <inheritdoc />
     public class InternalCompressor : Compressor
     {
-        /// <summary>
-        ///     Creates a DEFLATE archive for the provided source Directory at the given target.
-        /// </summary>
-        /// <param name="target">
-        ///     Target archive which will contain the compressed source Directory.
-        /// </param>
-        /// <param name="source">
-        ///     Source Directory on the filesystem to compress to the target archive.
-        /// </param>
+        /// <inheritdoc />
         public override void Compress(File target, Directory source)
         {
             ZipFile.CreateFromDirectory(source, target);
         }
 
-        /// <summary>
-        ///     Creates a DEFLATE archive for the provided files in source Directory at the given target.
-        /// </summary>
-        /// <param name="target">
-        ///     Target archive which will contain the compressed source Directory.
-        /// </param>
-        /// <param name="source">
-        ///     Source Directory on the filesystem containing the files.
-        /// </param>
-        /// <param name="files">
-        ///     Files in the source Directory to compress to the target archive.
-        /// </param>
+        /// <inheritdoc />
         public override void Compress(File target, Directory source, IEnumerable<File> files)
         {
             using (var zip = ZipFile.Open(target, ZipArchiveMode.Create))
