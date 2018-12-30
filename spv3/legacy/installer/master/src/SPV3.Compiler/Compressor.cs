fab2d6d04c5058e80999c3b3c893224078b0a12f
Emilian Roman <roman.emilian@outlook.com>
2018-12-30 13:41:59 +0800

    Update Compressor to natively compress instead of invoking 7-Zip

--- spv3/legacy/installer/master/src/SPV3.Compiler/Compressor.cs
+++ spv3/legacy/installer/master/src/SPV3.Compiler/Compressor.cs
@@ -1,10 +1,6 @@
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Security;
-using System.Security.Cryptography;
-using System.Text;
+using System.IO.Compression;
 using Directory = SPV3.Domain.Directory;
 using File = SPV3.Domain.File;
 
@@ -19,16 +15,6 @@ namespace SPV3.Compiler
     public class Compressor
     {
         /// <summary>
-        ///     Expected path for the 7-Zip executable, relative to the this DLL/Executable path.
-        /// </summary>
-        private static readonly string SevenZipPath = Path.Combine("7z", "7za.exe");
-
-        /// <summary>
-        ///     MD5 hash of the 7-Zip 18.05 (2018-04-30) (x64) (Windows) executable.
-        /// </summary>
-        private static readonly string SevenZipHash = "e93c0c45d84f328afba850e87ac7398c";
-
-        /// <summary>
         ///     Creates a DEFLATE archive for the provided source Directory at the given target.
         /// </summary>
         /// <param name="target">
@@ -39,9 +25,7 @@ namespace SPV3.Compiler
         /// </param>
         public void Compress(File target, Directory source)
         {
-            var args = new StringBuilder($"a -tzip \"{(string) target}\" \"{(string) source}\"");
-
-            InvokeProcess(args.ToString());
+            ZipFile.CreateFromDirectory(source, target);
         }
 
         /// <summary>
@@ -58,78 +42,12 @@ namespace SPV3.Compiler
         /// </param>
         public void Compress(File target, Directory source, IEnumerable<File> files)
         {
-            var args = new StringBuilder($"a -tzip \"{(string) target}\" ");
-
-            foreach (var file in files)
+            using (var zip = ZipFile.Open(target, ZipArchiveMode.Create))
             {
-                var path = Path.Combine(source, file);
-                args.Append($"\"{path}\" ");
-            }
+                const CompressionLevel level = CompressionLevel.Optimal;
 
-            InvokeProcess(args.ToString());
-        }
-
-        /// <summary>
-        ///     Safely invokes the 7-Zip executable with the provided arguments.
-        /// </summary>
-        /// <param name="args">
-        ///     Arguments to pass onto the 7-Zip executable. See: https://sevenzip.osdn.jp/chm/cmdline/
-        /// </param>
-        /// <param name="waitForExit">
-        ///     Hangs the thread until the process is finished. By default, this is true.
-        ///     This is useful in loop contexts where invoking one process at a time is preferable.
-        /// </param>
-        /// <exception cref="FormatException">
-        ///     Could not infer working directory.
-        ///     - or -
-        ///     Could not infer executable binary.
-        /// </exception>
-        /// <exception cref="SecurityException">
-        ///     7z executable hash does not match the expected one.
-        /// </exception>
-        private void InvokeProcess(string args, bool waitForExit = true)
-        {
-            var workingDirectory = Path.GetDirectoryName(SevenZipPath) ??
-                                   throw new FormatException("Could not infer working directory.");
-
-            var executableBinary = Path.GetFileName(SevenZipPath) ??
-                                   throw new FormatException("Could not infer executable binary.");
-
-            if (!VerifyExecutable())
-                throw new SecurityException("7z executable hash does not match the expected one.");
-
-            var process = new Process
-            {
-                StartInfo =
-                {
-                    FileName = executableBinary,
-                    WorkingDirectory = workingDirectory,
-                    Arguments = args
-                }
-            };
-
-            process.Start();
-
-            if (waitForExit)
-                process.WaitForExit();
-        }
-
-        /// <summary>
-        ///     Verifies the hash of the 7-Zip executable.
-        /// </summary>
-        /// <returns>
-        ///     True on the executable hash matching the expected one; otherwise false.
-        /// </returns>
-        private bool VerifyExecutable()
-        {
-            using (var md5 = MD5.Create())
-            using (var stream = System.IO.File.OpenRead(SevenZipPath))
-            {
-                return BitConverter
-                    .ToString(md5.ComputeHash(stream))
-                    .Replace("-", "")
-                    .ToLowerInvariant()
-                    .Equals(SevenZipHash);
+                foreach (var file in files)
+                    zip.CreateEntryFromFile(Path.Combine(source, file), file, level);
             }
         }
     }
