6c04ce5bf620f4d85c69ac417a58b9a8f8971745
Emilian Roman <roman.emilian@outlook.com>
2019-01-03 13:30:32 +0800

    Initialise the domain types

---
+++ spv3/legacy/resume/src/SPV3.Resume/SavegameRepository.cs
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2019 Emilian Roman
+ * 
+ * This file is part of SPV3.Resume.
+ * 
+ * SPV3.Resume is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SPV3.Resume is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SPV3.Resume.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+
+namespace SPV3.Resume
+{
+    public class SavegameRepository
+    {
+        private string _path;
+
+        public SavegameRepository(string path)
+        {
+            _path = path;
+        }
+
+        public void Save(Savegame savegame)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Savegame Load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
