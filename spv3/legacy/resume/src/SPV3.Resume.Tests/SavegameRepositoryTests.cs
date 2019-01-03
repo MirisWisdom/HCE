20c81dcf1ac82574739d832337830a13b51c495c
Emilian Roman <roman.emilian@outlook.com>
2019-01-03 18:09:50 +0800

    Implement Savegame Repository unit tests & Savegame raw example data

---
+++ spv3/legacy/resume/src/SPV3.Resume.Tests/SavegameRepositoryTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using SPV3.Domain;
+
+namespace SPV3.Resume.Tests
+{
+    [TestFixture]
+    public class SavegameRepositoryTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            System.IO.File.WriteAllBytes(Binary, Savegame.RawData);
+            _savegame = new SavegameRepository(Binary).Load();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            System.IO.File.Delete(Binary);
+        }
+
+        private static readonly File Binary = (File) "savegame.bin";
+        private static Resume.Savegame _savegame;
+
+        [Test]
+        public void LoadSavegame_DifficultyIsCorrect_True()
+        {
+            Assert.AreEqual(Difficulty.Legendary, _savegame.Difficulty);
+        }
+
+        [Test]
+        public void LoadSavegame_MissionIsCorrect_True()
+        {
+            Assert.AreEqual(Mission.Spv3a30, _savegame.Mission);
+        }
+    }
+}
