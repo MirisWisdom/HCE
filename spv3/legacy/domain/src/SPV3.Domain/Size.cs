00306346a64bb58b2f26a6d56d5123c74c3491f2
Emilian Roman <roman.emilian@outlook.com>
2019-01-04 15:13:41 +0800

    Introduce the Entry Size property

---
+++ spv3/legacy/domain/src/SPV3.Domain/Size.cs
@@ -0,0 +1,13 @@
+namespace SPV3.Domain
+{
+    /// <summary>
+    ///     Binary file size.
+    /// </summary>
+    public class Size
+    {
+        /// <summary>
+        ///     Value of the binary file size.
+        /// </summary>
+        public int Value { get; set; }
+    }
+}
