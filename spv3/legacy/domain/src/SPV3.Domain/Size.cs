7a8d88e91af2a3c55eb1dd63bc78e2b73dbc2064
Emilian Roman <roman.emilian@outlook.com>
2019-01-04 15:15:59 +0800

    Make Size Value long, and introduce implicit & explicit operators for it

--- spv3/legacy/domain/src/SPV3.Domain/Size.cs
+++ spv3/legacy/domain/src/SPV3.Domain/Size.cs
@@ -8,6 +8,37 @@ namespace SPV3.Domain
         /// <summary>
         ///     Value of the binary file size.
         /// </summary>
-        public int Value { get; set; }
+        public long Value { get; set; }
+
+        /// <summary>
+        ///     Represent object as long.
+        /// </summary>
+        /// <param name="size">
+        ///     Object to represent as long.
+        /// </param>
+        /// <returns>
+        ///     Long representation of the object.
+        /// </returns>
+        public static implicit operator long(Size size)
+        {
+            return size.Value;
+        }
+
+        /// <summary>
+        ///     Represent long as object.
+        /// </summary>
+        /// <param name="value">
+        ///     Long to represent as object.
+        /// </param>
+        /// <returns>
+        ///     Object representation of the long.
+        /// </returns>
+        public static explicit operator Size(long value)
+        {
+            return new Size
+            {
+                Value = value
+            };
+        }
     }
 }
